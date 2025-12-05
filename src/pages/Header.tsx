98b6c36c703aa0947c1a9e017c32f02724c77252
Sahani26 <sunilsahani484@gmail.com>
2025-12-05 23:30:35 +0530

    Initial commit

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -2,132 +2,91 @@
 
 import { useEffect } from "react";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/img/header-logo.png";
+import { usePathname } from "next/navigation";
+
+
 
 export default function Header() {
+    const pathname = usePathname();
 
-useEffect(() => {
-    const btn = document.querySelector(".navbar-toggler");
-    const menu = document.querySelector("#navbarCollapse");
-    const links = document.querySelectorAll("#navbarCollapse .nav-link, #navbarCollapse .dropdown-item");
+    useEffect(() => {
+        const btn = document.querySelector(".navbar-toggler");
+        const menu = document.querySelector("#navbarCollapse");
+        const remove = document.querySelector(".close-buttton");
+        const overlay = document.querySelector(".overlay");
+        const links = document.querySelectorAll("#navbarCollapse .nav-link, #navbarCollapse .dropdown-item");
 
-    if (!btn || !menu) return;
+        if (!btn || !menu || !overlay) return;
 
-    // Open/Close toggle
-    btn.addEventListener("click", () => {
-        menu.classList.toggle("show");
-    });
+        // Functions
+        const openCloseMenu = () => {
+            menu.classList.toggle("show");
 
-    // Close when clicking any menu item
-    links.forEach(link => {
-        link.addEventListener("click", () => {
+            if (menu.classList.contains("show")) {
+                overlay.classList.add("show");  // add active class
+                document.body.classList.add("show");  // add active class
+            } else {
+                overlay.classList.remove("show"); // remove when closed
+                document.body.classList.remove("show"); // remove when closed
+            }
+        };
+
+        const closeMenu = () => {
             menu.classList.remove("show");
+            document.body.classList.remove("show"); // remove from body
+            overlay.classList.remove("show");
+        };
+
+        // Open/Close toggle
+        btn.addEventListener("click", openCloseMenu);
+
+        // Close button
+        if (remove) {
+            remove.addEventListener("click", closeMenu);
+        }
+
+        // Close when clicking any menu item
+        links.forEach(link => {
+            link.addEventListener("click", closeMenu);
         });
-    });
 
-    return () => {
-        btn.removeEventListener("click", () => {});
-        links.forEach(link => link.removeEventListener("click", () => {}));
-    };
-}, []);
+        // Cleanup
+        return () => {
+            btn.removeEventListener("click", openCloseMenu);
+            if (remove) remove.removeEventListener("click", closeMenu);
+            links.forEach(link => link.removeEventListener("click", closeMenu));
+        };
+    }, []);
 
-
-    
     return (
         <div className="container-fluid header-top">
             <div className="container d-flex align-items-center">
-                
+
                 {/* Left Logo (Big Screen) */}
                 <div className="d-flex align-items-center h-100">
-                    <Link href="/" className="navbar-brand" style={{ height: "125px" }}>
+                    <Link href="/" className="navbar-brand"  >
                         <h1 className="text-primary mb-0">
-                            <i className="fas fa-bolt"></i>CoolPower
+                            <Image src={Logo} alt="General Electrical" className="logo-img img-fluid w-100" />
                         </h1>
                     </Link>
                 </div>
 
-                <div className="w-100 h-100">
+                <div className="toggle-header">
 
                     {/* Topbar */}
-                    <div 
-                        className="topbar px-0 py-2 d-none d-lg-block" 
-                        style={{ height: "45px" }}
-                    >
-                        <div className="row gx-0 align-items-center">
 
-                            {/* Left Info */}
-                            <div className="col-lg-8 text-center text-lg-center mb-lg-0">
-                                <div className="d-flex flex-wrap">
-                                    <div className="border-end border-primary pe-3">
-                                        <Link href="#" className="text-muted small">
-                                            <i className="fas fa-map-marker-alt text-primary me-2"></i>
-                                            Find A Location
-                                        </Link>
-                                    </div>
-
-                                    <div className="ps-3">
-                                        <a href="mailto:example@gmail.com" className="text-muted small">
-                                            <i className="fas fa-envelope text-primary me-2"></i>
-                                            example@gmail.com
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* Right Social + Language */}
-                            <div className="col-lg-4 text-center text-lg-end">
-                                <div className="d-flex justify-content-end">
-
-                                    <div className="d-flex border-end border-primary pe-3">
-                                        <Link href="#" className="btn p-0 text-primary me-3">
-                                            <i className="fab fa-facebook-f"></i>
-                                        </Link>
-                                        <Link href="#" className="btn p-0 text-primary me-3">
-                                            <i className="fab fa-twitter"></i>
-                                        </Link>
-                                        <Link href="#" className="btn p-0 text-primary me-3">
-                                            <i className="fab fa-instagram"></i>
-                                        </Link>
-                                        <Link href="#" className="btn p-0 text-primary me-0">
-                                            <i className="fab fa-linkedin-in"></i>
-                                        </Link>
-                                    </div>
-
-                                    {/* Language Dropdown */}
-                                    <div className="dropdown ms-3">
-                                        <a
-                                            href="#"
-                                            className="dropdown-toggle text-white"
-                                            data-bs-toggle="dropdown"
-                                        >
-                                            <small className="text-body">
-                                                <i className="fas fa-globe-europe text-primary me-2"></i>
-                                                English
-                                            </small>
-                                        </a>
-
-                                        <div className="dropdown-menu">
-                                            <Link className="dropdown-item" href="#">English</Link>
-                                            <Link className="dropdown-item" href="#">Bangla</Link>
-                                            <Link className="dropdown-item" href="#">French</Link>
-                                            <Link className="dropdown-item" href="#">Spanish</Link>
-                                            <Link className="dropdown-item" href="#">Arabic</Link>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
 
                     {/* Navbar */}
-                    <div className="nav-bar px-0 py-lg-0" style={{ height: "80px" }}>
+                    <div className="nav-bar px-0 py-lg-0" >
                         <nav className="navbar navbar-expand-lg navbar-light d-flex justify-content-lg-end">
 
                             {/* Logo for Mobile */}
-                            <Link href="#" className="navbar-brand-2">
+                            <div className="overlay"></div>
+                            <Link href="/" className="navbar-brand-2">
                                 <h1 className="text-primary mb-0">
-                                    <i className="fas fa-bolt"></i>CoolPower
+                                    <Image src={Logo} alt="General Electrical" className="img-fluid w-100" />
                                 </h1>
                             </Link>
 
@@ -142,22 +101,31 @@ useEffect(() => {
                             </button>
 
                             <div className="collapse navbar-collapse" id="navbarCollapse">
-                                <div className="navbar-nav mx-0 mx-lg-auto bg-white">
 
-                                    <Link href="/" className="nav-item nav-link active">
+                                <div className="navbar-nav mx-0 mx-lg-auto bg-white">
+                                    <div className="close_bt">
+                                        <div className="close-buttton"></div>
+
+
+                                    </div>
+                                    <Link href="/" className={`nav-item nav-link ${pathname === "/" ? "active" : ""}`}>
                                         Home
                                     </Link>
 
-                                    <Link href="/about" className="nav-item nav-link">
+                                    <Link href="/about" className={`nav-item nav-link ${pathname === "/about" ? "active" : ""}`}>
                                         About
                                     </Link>
 
-                                    <Link href="/service" className="nav-item nav-link">
+                                    <Link href="/service" className={`nav-item nav-link ${pathname === "/service" ? "active" : ""}`}>
                                         Service
                                     </Link>
-                                    <Link href="/blog" className="nav-item nav-link">
+
+                                    <Link href="/blog" className={`nav-item nav-link ${pathname === "/blog" ? "active" : ""}`}>
                                         Blog
                                     </Link>
+
+
+
 
                                     {/* Dropdown Pages */}
                                     <div className="nav-item dropdown">
@@ -165,17 +133,34 @@ useEffect(() => {
                                             <span className="dropdown-toggle">Pages</span>
                                         </a>
                                         <div className="dropdown-menu">
-                                            <Link href="/team" className="dropdown-item">Our Team</Link>
-                                            <Link href="/testimonial" className="dropdown-item">Testimonial</Link>
-                                            <Link href="/404" className="dropdown-item">404 Page</Link>
+                                            <Link
+                                                href="/team"
+                                                className={`dropdown-item ${pathname === "/team" ? "active" : ""}`}
+                                            >
+                                                Our Team
+                                            </Link>
+
+                                            <Link
+                                                href="/testimonial"
+                                                className={`dropdown-item ${pathname === "/testimonial" ? "active" : ""}`}
+                                            >
+                                                Testimonial
+                                            </Link>
+
+                                            <Link
+                                                href="/404"
+                                                className={`dropdown-item ${pathname === "/404" ? "active" : ""}`}
+                                            >
+                                                404 Page
+                                            </Link>
                                         </div>
                                     </div>
 
-                                    <Link href="/contact" className="nav-item nav-link">
+                                    <Link href="/contact" className={`nav-item nav-link ${pathname === "/contact" ? "active" : ""}`}>
                                         Contact
                                     </Link>
 
-                                   
+
 
                                 </div>
                             </div>
